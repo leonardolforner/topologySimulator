fe0947a1673836c904508a896763b84747f3a22a
Leonardo Forner <leonardo.forner@sap.com>
2025-09-30 17:31:50 -0300

    test bullshit main thing

---
+++ cmd/sim/main.go
@@ -0,0 +1,29 @@
+package sim
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"leonardolforner/topologySimulator/topologySimulator/internal/config"
+)
+
+func main() {
+	path := flag.String("config", "topology.yaml", "YAML file")
+	flag.Parse()
+
+	cfg, err := config.Load(*path)
+	if err != nil {
+		log.Fatalf("load: %v", err)
+	}
+
+	fmt.Println("Queues loaded:")
+	for name, q := range cfg.Queues {
+		capStr := "∞"
+		if q.Capacity != nil {
+			capStr = fmt.Sprint(*q.Capacity)
+		}
+		fmt.Printf("- %s: servers=%d cap=%s svc=[%.1f..%.1f]\n",
+			name, q.Servers, capStr, q.MinService, q.MaxService)
+	}
+}
