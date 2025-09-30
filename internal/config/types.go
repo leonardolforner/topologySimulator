aba71535369e78a74ce31af4773c46a7c628073e
Leonardo Forner <leonardo.forner@sap.com>
2025-09-30 16:15:13 -0300

    add yaml schema mapping

--- internal/config/types.go
+++ internal/config/types.go
@@ -1,25 +1,25 @@
 package config
 
 type Config struct {
-	Arrivals          map[string]float64
-	Queues            map[string]Queue
-	Network           []Edge
-	RndNumbers        []float64
-	RndNumbersPerSeed int
-	Seeds             []int64
+	Arrivals          map[string]float64 `yaml:"arrivals"`
+	Queues            map[string]Queue   `yaml:"queues"`
+	Network           []Edge             `yaml:"network"`
+	RndNumbers        []float64          `yaml:"rndnumbers"`
+	RndNumbersPerSeed int                `yaml:"rndnumbersperseed"`
+	Seeds             []int64            `yaml:"seeds"`
 }
 
 type Queue struct {
-	Servers    int
-	Capacity   *int // nil == no maximum
-	MinArrival *float64
-	MaxArrival *float64
-	MinService float64
-	MaxService float64
+	Servers    int      `yaml:"servers"`
+	Capacity   *int     `yaml:"capacity"` // nil == no maximum
+	MinArrival *float64 `yaml:"minArrival"`
+	MaxArrival *float64 `yaml:"maxArrival"`
+	MinService float64  `yaml:"minService"`
+	MaxService float64  `yaml:"maxService"`
 }
 
 type Edge struct {
-	Source      string
-	Target      string
-	Probability float64
+	Source      string  `yaml:"source"`
+	Target      string  `yaml:"target"`
+	Probability float64 `yaml:"probability"`
 }
